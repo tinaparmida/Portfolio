dbd05d00568998b25a0831ac4c6fd756011899ec
tinaparmida <113638340+tinaparmida@users.noreply.github.com>
2023-04-21 15:29:59 -0400

    Add files via upload

---
+++ attributes.js
@@ -0,0 +1,130 @@
+/**
+attributes.js
+ *
+ *
+ */
+
+window.products = [
+  {
+    id: "A1",
+    title: "GitHub",
+    description:
+      "Through working on large scale programming projects, I have gained proficiency in Git version control. I am experience collaborating on software projects using GitHub. I am familiar with GitHub features such as pull requests and code reviews.",
+
+    categories: ["skills"],
+    imageUrl:
+      "https://images.unsplash.com/photo-1556075798-4825dfaaf498?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2l0aHVifGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60"
+  },
+  {
+    id: "A2",
+    title: "Javascript",
+    description:
+      "I have experience developing web applications and interactive user interfaces using JavaScript. This gives me a strong understanding of fundamental concepts such as variables, data types, control structures, functions, and event handling.",
+
+    categories: ["skills"],
+    imageUrl:
+      "https://images.unsplash.com/photo-1597239451127-914cc6d50a1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjZ8fGphdmFzY3JpcHR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    id: "A3",
+    title: "MySQL",
+    description:
+      "I am skilled in designing and implementing MySQL databases. I am proficient in writing optimized SQL queries for data retrieval and manipulation, and comfortable implementing business rules to design and create normalized databases.",
+
+    categories: ["skills"],
+    imageUrl:
+      "https://images.unsplash.com/photo-1483736762161-1d107f3c78e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZGF0YWJhc2V8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60"
+  },
+  {
+    id: "A4",
+    title: "Python",
+    description:
+      "Thus far, I have limited experience developing applications and software using Python. However, I can comfortably create small projects with understanding of fundamental concepts such as variables, data types, control structures, functions, and modules.",
+
+    categories: ["skills"],
+    imageUrl:
+      "https://images.unsplash.com/photo-1538439907460-1596cafd4eff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHl0aG9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60"
+  },
+
+  {
+    id: "A5",
+    title: "UofT BSc",
+    description:
+      "My passion for science initially took me to University of Toronto, where I completed my Bachelor of Science Degree in Pharmacology and Human Biology. I graduated Honours with Distinction from the St. George campus in 2021.",
+
+    categories: ["education"],
+    imageUrl:
+      "https://images.unsplash.com/photo-1565333867024-469d96983aab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dW5pdmVyc2l0eSUyMG9mJTIwdG9yb250b3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60"
+  },
+  {
+    id: "A6",
+    title: "CPA",
+    description:
+      "I am currently enrolled in Computer Programming and Analysis graduate certificate at Seneca School of Software Design and Data Science. Here I hope to bring together my passion for science and technology. Expected Graduation: 2025",
+
+    categories: ["education"],
+    imageUrl:
+      "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cHl0aG9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60"
+  },
+  {
+    id: "A7",
+    title: "HTML, CSS",
+    description:
+      "I have experience designing and developing responsive web pages and user interfaces using HTML and CSS. I am also proficient in using CSS preprocessors like SASS and LESS, as well as CSS frameworks like Bootstrap and Materialize.",
+
+    categories: ["skills"],
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1661679327649-365865b01020?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8amF2YXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60"
+  },
+  {
+    id: "A9",
+    title: "Data Analysis",
+    description:
+      "When working in Administration, I became interested in Data Analysis. I obtained the Google Career Certificate in Data Analysis in 2022, teaching me statistical analysis and data visualization to extract meaningful insights and communicate findings.",
+
+    categories: ["education"],
+    imageUrl:
+      "https://images.unsplash.com/photo-1543286386-713bdd548da4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YW5hbHlzaXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    id: "A10",
+    title: "C, C++",
+    description:
+      "I am experienced in developing applications and software using C and C++. C++ in particular has given me a strong understanding of object-oriented programming (OOP) concepts such as classes, inheritance, polymorphism, and templates.",
+
+    categories: ["skills"],
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1669749353978-d1d8feffd041?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    id: "A11",
+    title: "Chess",
+    description:
+      "Every since I was 6, I recall having a strong passion for the game of chess. I have been an avid chess player since. There is something quite special cognitive skills and strategic thinking that are developed through playing this game.",
+
+    categories: ["hobbies"],
+    imageUrl:
+      "https://images.unsplash.com/photo-1617745143864-3907eaa93603?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjB8fGNoZXNzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    id: "A12",
+    title: "Aviation",
+    description:
+      "One of my main hobbies derives from my passion for aviation. I enjoy learning about different aircraft types and their capabilities. I hope to be working in the field one day, particularly to develope software for the aviation industry.",
+
+    categories: ["hobbies"],
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1679830513876-999c0189be26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODV8fHBsYW5lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+  },
+
+  {
+    id: "A16",
+    title: "Cat mom",
+    description:
+      "I have immense love for animals and am passionate about animal welfare. I have a lovely cat named Tsuki who I rescued from the shelter when he was just 2 months old. I can't believe he will be turning 3 years old (that's 21 human years!) this coming August!",
+
+    categories: ["hobbies"],
+    imageUrl:
+      "https://images.unsplash.com/photo-1615000363959-e52262d2c75b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsYWNrJTIwY2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+  }
+];
