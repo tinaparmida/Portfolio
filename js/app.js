88bee0721fbb1440d9a0f0a526c149f295f33679
tinaparmida <113638340+tinaparmida@users.noreply.github.com>
2023-06-06 11:43:29 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -73,7 +73,7 @@ function createHomeProductCard(product) {
   productImage.alt = product.title;
   productImage.classList.add("home-card-image");
   productImage.style.width = "200px";
-  productImage.style.height = "200px";
+  productImage.style.height = "232.51px";
   card.appendChild(productImage);
 
   // Return the card's <div> element to the caller
